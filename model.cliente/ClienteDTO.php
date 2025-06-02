503deb497b1f440f609bbfe63e83f8ec46422515
Christian12 <crisp314528@gmail.com>
2025-06-02 14:26:21 -0600

    Implementación de ClienteDTO
    Clase que representa el modelo de datos del cliente con sus atributos principales.

---
+++ model.cliente/ClienteDTO.php
@@ -0,0 +1,11 @@
+<?php
+// Archivo: model.cliente/ClienteDTO.php
+class ClienteDTO
+{
+    public $id;
+    public $nombre;
+    public $correo;
+    public $telefono;
+    public $lugarResidencia;
+    public $fechaCumpleanos;
+}

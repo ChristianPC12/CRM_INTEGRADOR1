bfb26d95f2467637c6ae51b20b8ec0820752fe1b
Christian12 <crisp314528@gmail.com>
2025-06-22 14:30:56 -0600

    Estructura para diseño de fondo de CompraView.php

---
+++ view/CompraView.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CompraView</title>
+</head>
+
+<body>
+    <div class="template-container template-1" data-template="1">
+    </div>
+
+</body>
+
+</html>

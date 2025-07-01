ba6009f58075cd3adc67e1a3090c508af40d58b2
ChristianPC12 <crisp314528@gmail.com>
2025-07-01 17:13:55 -0600

    variables mapeadas en la bd de la tabla codigo

--- model/codigo/CodigoMapper.php
+++ model/codigo/CodigoMapper.php
@@ -1,5 +1,6 @@
 <?php
-// Archivo: model/codigo/CodigoMapper.php
+// model/codigo/CodigoMapper.php
+
 require_once 'CodigoDTO.php';
 
 class CodigoMapper
@@ -9,7 +10,7 @@ class CodigoMapper
         $dto = new CodigoDTO();
         $dto->id = $row['Id'];
         $dto->idCliente = $row['IdCliente'];
-        $dto->fechaRegistro = $row['FechaRegistro'];
+        $dto->codigoBarra = $row['CodigoBarra'];
         $dto->cantImpresiones = $row['CantImpresiones'];
         return $dto;
     }

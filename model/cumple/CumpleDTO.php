66b30ee2cca6297f0dc0ea1fbe918bc503453f84
Reyman <rpapillo27@gmail.com>
2025-07-10 18:41:49 -0600

    DTO cumpleaños

---
+++ model/cumple/CumpleDTO.php
@@ -0,0 +1,12 @@
+<?php
+
+class CumpleDTO
+{
+    public $id;
+    public $cedula;
+    public $nombre;
+    public $correo;
+    public $telefono;
+    public $fechaCumpleanos;
+    public $estado = 'PENDIENTE'; // Por defecto
+}

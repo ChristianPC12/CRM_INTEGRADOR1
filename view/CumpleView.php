4525b9a3b1cbdc66e2f8bd7265c27244fa403c15
Reyman <rpapillo27@gmail.com>
2025-07-10 18:40:08 -0600

    Vista del cumpleaños

---
+++ view/CumpleView.php
@@ -0,0 +1,15 @@
+<div class="card shadow mb-4">
+    <div class="card-header d-flex justify-content-between align-items-center bg-dark text-white">
+        <h5 class="mb-0">Cumpleaños de la Semana</h5>
+    </div>
+    <div class="card-body p-0">
+        <div class="table-responsive">
+            <div id="cumpleLista" class="p-4">
+                <!-- Aquí se inyectará la tabla desde JS -->
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/CRM_INT/CRM/public/js/Cumple.js?v=<?= time() ?>"></script>
+<link rel="stylesheet" href="/CRM_INT/CRM/public/css/Cumple.css">

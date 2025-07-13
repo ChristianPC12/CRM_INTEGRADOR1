ca08c81372f10dad1590a0b90b664dbd2725db16
Felipe0619 <randallfelipe9906@gmail.com>
2025-07-13 17:15:29 -0600

    BitacoraDTO

---
+++ model/Bitacora/BitacoraDTO.php
@@ -0,0 +1,11 @@
+<?php
+class BitacoraDTO {
+    public $id;
+    public $idUsuario;
+    public $usuario;
+    public $rol;
+    public $horaEntrada;
+    public $horaSalida;
+    public $duracion;
+    public $fecha;
+}

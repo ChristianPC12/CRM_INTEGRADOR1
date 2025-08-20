c943c3a67df401bdb8085c241d2d41884ef3a32e
ChristianPC12 <crisp314528@gmail.com>
2025-08-20 02:18:40 -0600

    cambio de link repetidos que bugueaban ambas secciones de manual

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -116,7 +116,7 @@ $vista = $_GET['view'] ?? 'dashboard';
             </li>
 
             <li>
-                <a href="/CRM_INT/CRM/index.php?view=guia2" id="link-guia"
+                <a href="/CRM_INT/CRM/index.php?view=guia2" id="link-guia2"
                     class="<?= $vista === 'guia2' ? 'active' : '' ?>">
                     <i class="bi bi-info-circle"></i> Manual de usuario
                 </a>

24f008986a5f8abf6791dfc426f5833809010801
Bryan Vega <bryanvega7891@gmail.com>
2025-08-20 20:08:38 -0600

    cambio icono de manual de usuario

--- view/LayoutView.php
+++ view/LayoutView.php
@@ -119,8 +119,8 @@ $vista = $_GET['view'] ?? 'dashboard';
 
             <li>
                 <a href="/CRM_INT/CRM/index.php?view=guia2" id="link-guia2"
-                    class="<?= $vista === 'guia2' ? 'active' : '' ?>">
-                    <i class="bi bi-info-circle"></i> Manual de usuario
+                        class="<?= $vista === 'guia2' ? 'active' : '' ?>">
+                        <i class="bi bi-person-badge"></i> Manual de usuario
                 </a>
             </li>
 
